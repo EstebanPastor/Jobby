6e15c1dab4c2f11dbbd62e42fb549d29fbf28d91
Esteban Pastor <estebanandrespastor@gmail.com>
2024-02-26 16:15:44 -0300

    feat: :lipstick: Add button component props and styling

--- components/ui/button/Button.tsx
+++ components/ui/button/Button.tsx
@@ -1,11 +1,35 @@
+"use client";
 
+import clsx from "clsx";
+import { useFormStatus } from "react-dom";
 
-const Button = ({children}) => {
-  return (
-    <button>
-        {children}
-    </button>
-  )
+interface ButtonProps {
+  type?: "button" | "submit" | "reset" | undefined;
+  children?: React.ReactNode;
+  onClick?: () => void;
+  fullWidth?: boolean;
 }
 
-export default Button
+const Button: React.FC<ButtonProps> = ({
+  children,
+  type,
+  onClick,
+  fullWidth,
+}) => {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      type={type}
+      disabled={pending}
+      onClick={onClick}
+      className={clsx(
+        `bg-purple-600 px-4 py-1 rounded-md text-white h-full transition duration-30 hover:bg-purple-600/75`,
+        fullWidth && `w-full`
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
